8fca79023e1e8f42f01490272117108e86457ae1
Jesniphat <jesniphat@hotmail.com>
2020-06-06 17:46:11 +0700

    add ngx-scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { HttpClientModule } from '@angular/common/http';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { FactService } from './_service/fact.service';
 import { FactScrollerComponent } from './fact-scroller/fact-scroller.component';
+import { NgxScrollerComponent } from './ngx-scroller/ngx-scroller.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FactScrollerComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    ScrollingModule
-  ],
-  providers: [FactService],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      FactScrollerComponent,
+      NgxScrollerComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      ScrollingModule,
+      InfiniteScrollModule
+   ],
+   providers: [
+      FactService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
---
+++ src/app/ngx-scroller/ngx-scroller.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FactService } from '../_service/fact.service';
+
+@Component({
+  selector: 'app-ngx-scroller',
+  templateUrl: './ngx-scroller.component.html',
+  styleUrls: ['./ngx-scroller.component.scss']
+})
+export class NgxScrollerComponent implements OnInit {
+  page = 0;
+  dataList = [];
+
+  constructor(private factService: FactService) { }
+
+  ngOnInit() {
+    this.facthValue();
+  }
+
+  public onScroll(ev: any) {
+    this.page += 1;
+    this.facthValue();
+  }
+
+  private facthValue() {
+    if (this.page >= 0) {
+      this.factService.getRandomFact('http://localhost:3000/api/paging?page=' + this.page).subscribe(res => {
+        console.log(res);
+        this.dataList = [...this.dataList, ...res];
+      });
+    }
+  }
+
+}
